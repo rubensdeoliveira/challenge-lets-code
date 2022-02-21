bae59d6a21c31037f98efed8be2f5bd07b9db20f
Jose Rubens <jose.rubens@uds.com.br>
2022-02-20 23:34:55 -0300

    fix: update port

--- BACK/index.js
+++ BACK/index.js
@@ -112,4 +112,4 @@ app.delete('/cards/:id', (req, res) => {
     res.json(cards);
 });
 
-app.listen(5001, () => console.log('listening on http://localhost:5001'));
+app.listen(5000, () => console.log('listening on http://localhost:5000'));
